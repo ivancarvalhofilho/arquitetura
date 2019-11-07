3ed8348be6e49d9419f5acb128bfe56ffa2f4ad9
ivancarvalhofilho <ivancarvalhofilho@github.com>
2019-11-06 23:11:22 -0200

    feito tudo

--- index.js
+++ index.js
@@ -1,150 +1,169 @@
-class BHT{
-	constructor(numeroBitsPC, n){
-		this.numeroBitsPC = numeroBitsPC;
-		this.address = Array();
-		this.pred = Array();
-		this.hist = Array();
-		this.hits = Array();
-		this.miss = Array();
-	}
-
-	executar(endereco, resultado){
-		let predicao = 0;
-		if(!this.address.includes(endereco)){
-			this.address.push(endereco);
-			this.pred.push(2);
-		}
-		let pos = ((parseInt(endereco, 16) >>> 2) % Math.pow(2,this.numeroBitsPC)) + 1;
-		console.log(pos);
-		predicao = this.pred[pos];
-		document.getElementById("myTable").rows[pos].cells[3].innerHTML=resultado;
-		let end = document.getElementById("myTable").rows[pos].cells[1].innerHTML;
-		console.log(end);
-		if(endereco !== end){
-			predicao=2;
-			this.pred[pos]=2;
-		}
-		console.log("Predicao: "+predicao);
-		console.log("Resultado: "+resultado);
-		if ((predicao <= 1 && resultado == 'N') || (predicao >= 2 && resultado == 'T')){
-			if(predicao == 1){
-				this.pred[pos] -= 1;
-			} else if (predicao == 2){
-				this.pred[pos] += 1;
-			}
-			this.hits[pos] += 1;
-		} else {
-			if(predicao == 0){
-				this.pred[pos] += 1;
-			} else if (predicao == 1){
-				this.pred[pos] += 2;
-			} else if (predicao == 2){
-				this.pred[pos] -= 2;
-			} else {
-				this.pred[pos] -= 1;
-			}
-			this.miss[pos] += 1;
-		}
-		if(this.pred[pos]==0){
-			this.hist[pos]="N,N";
-		}
-		else if(this.pred[pos]==1){
-			this.hist[pos]="N,T";
-		}
-		else if(this.pred[pos]==2){
-			this.hist[pos]="T,N";
-		}
-		else{
-			this.hist[pos]="T,T";
-		}
-		document.getElementById("myTable").rows[pos].cells[1].innerHTML=endereco;
-		(predicao<=1) ? predicao="NT" : predicao="T";
-		document.getElementById("myTable").rows[pos].cells[4].innerHTML=predicao;
-		document.getElementById("myTable").rows[pos].cells[2].innerHTML= this.hist[pos];
-	}
-
-	getPercent(){
-		return parseInt(this.hits/(this.hits + this.miss)*100);
-	}
-
-	getHits(){
-		return this.hits;
-	}
-
-	getMiss(){
-		return this.miss;
-	}
-}
-
-let bht;
-let linhasDoArquivo = Array();
-let linhaAtualIndice = -1;
-let linhaAtual;
-let flagFimDoArquivo = false;
+let linhasDoArquivo = [];
+let linhaAtualIndice = 0;
+let numeroBitsPreditor;
+let numeroBitsPC;
 
 function lerArquivo(){
-	let file = $('#file-id').prop('files')[0];;
+	let file = $('#file-id').prop('files')[0];
 	let reader = new FileReader();
 	reader.onload = function(){
-		linhaAtualIndice = -1;
 		linhasDoArquivo = this.result.split('\n');
+		numeroAcertosPorLinha = new Array
 	};
 	reader.readAsText(file);
 }
 
-function executaProximaLinha(){ 
+function executarProximaLinha(){
 	linhaAtual = linhasDoArquivo[linhaAtualIndice].split(' ');
-	bht.executar(linhaAtual[0], linhaAtual[1]);
-}
-
-function iniciarPreditor(){
-	$(".menu").hide();
-	$("#menu-2").show();
-	
-	let table = $("#myTable")[0];
-	let numeroBitsPC = $("#numeroBitsPC").val();
-	let n = $("#n").val();
-	let m2 = 1;
-
-	lerArquivo();
-	
-	bht = new BHT(numeroBitsPC, n);
-	
-	for (let i=0; i<numeroBitsPC; i++){
-		m2*=2;
-	}
-	
-	for (let i=0; i<m2; i++){
-		let row = table.insertRow(table.length);
-		let cell1 = row.insertCell(0);
-		let cell2 = row.insertCell(1);
-		let cell3 = row.insertCell(2);
-		let cell4 = row.insertCell(3);
-		let cell5 = row.insertCell(4);
-		
-		let e = 0;
-		let aux = i;
-		
-		for (let j=0; j<numeroBitsPC; j++){
-			e += aux % 2 * Math.pow(10,numeroBitsPC);
-			aux=parseInt(aux/2);
-			e=parseInt(e/10);
-		}
-		
-		cell1.innerHTML = e;
-		cell2.innerHTML = "";
-		cell3.innerHTML = "T,N";
-		cell4.innerHTML = "";
-		cell5.innerHTML = "";
-	}
+	executar(linhaAtual[0], linhaAtual[1]);
 }
 
 function proximaExecutacao(){
-	linhaAtualIndice += 1;
-	if (linhaAtualIndice >= linhasDoArquivo.length){
-		flagFimDoArquivo = true;
+	if (linhaAtualIndice < linhasDoArquivo.length - 1){
+		executarProximaLinha();
+		linhaAtualIndice += 1;
 	} else {
-		executaProximaLinha();
-		flagFimDoArquivo = false;
+		$("#botaoDoNext").disabled = true;
+		$("#botaoDoNext").text("Finish");
 	}
+}
+
+function iniciarPreditor () {
+	lerArquivo();
+	
+	$("#menu-1").hide();
+	$("#menu-2").show();
+
+	let table = $("#myTable")[0];
+	numeroBitsPC = $("#numeroBitsPC").val();
+	let quantidadeLinhasNaTabela = Math.pow(2, numeroBitsPC);
+	
+	numeroBitsPreditor = $("#numeroBitsPreditor").val();
+
+	let colunas = [];
+	for (let i=0; i < quantidadeLinhasNaTabela; i++){
+		let row = table.insertRow(table.length);
+		
+		for (let j = 0; j < 9; j++) {
+			colunas[j] = row.insertCell(j);
+		}
+
+		colunas[COLUNA.ID].innerHTML = i.toString(2);
+		colunas[COLUNA.ENDERECO].innerHTML = "";
+		colunas[COLUNA.HISTORICO].innerHTML = numeroBitsPreditor == 1 ? "T" : "T,N";
+		colunas[COLUNA.REALIZADO].innerHTML = "";
+		colunas[COLUNA.PREDITO].innerHTML = "";
+		colunas[COLUNA.DESVIO].innerHTML = "";
+		colunas[COLUNA.ACERTOS].innerHTML = 0;
+		colunas[COLUNA.TOTAL].innerHTML = 0;
+		colunas[COLUNA.PRECISAO].innerHTML = 0;
+	}
+}
+
+COLUNA = {
+	ID: 0,	
+	ENDERECO: 1,	
+	HISTORICO: 2,	
+	REALIZADO: 3,	
+	PREDITO: 4,
+	DESVIO: 5,
+	ACERTOS: 6,
+	TOTAL: 7,
+	PRECISAO: 8
+};
+
+function inverteTomado( valor ) {
+	return valor == "T" ? "N" : "T";
+}
+
+function calculaPrecisao(linha) {
+	return ((linha.cells[COLUNA.ACERTOS].innerHTML / linha.cells[COLUNA.TOTAL].innerHTML) * 100).toFixed(2).toString() + "%";
+}
+
+function preditor1Bit(linha, realizado) {
+	if (linha.cells[COLUNA.HISTORICO].innerHTML == realizado) {
+		linha.cells[COLUNA.PREDITO].innerHTML = realizado;
+		linha.cells[COLUNA.DESVIO].innerHTML = "Certo";
+		linha.cells[COLUNA.ACERTOS].innerHTML++;
+	} else {
+		linha.cells[COLUNA.PREDITO].innerHTML = linha.cells[COLUNA.HISTORICO].innerHTML;
+		linha.cells[COLUNA.HISTORICO].innerHTML = inverteTomado(linha.cells[COLUNA.HISTORICO].innerHTML);
+		linha.cells[COLUNA.DESVIO].innerHTML = "Errado";
+	}
+	linha.cells[COLUNA.TOTAL].innerHTML++;
+	linha.cells[COLUNA.PRECISAO].innerHTML = calculaPrecisao(linha);
+}
+
+function preditor2Bits(linha, realizado) {
+	if (linha.cells[COLUNA.HISTORICO].innerHTML == "T,T") {
+		if (realizado == "T") {
+			linha.cells[COLUNA.PREDITO].innerHTML = realizado;
+			linha.cells[COLUNA.DESVIO].innerHTML = "Certo";
+			linha.cells[COLUNA.ACERTOS].innerHTML++;
+		} else {
+			linha.cells[COLUNA.PREDITO].innerHTML = inverteTomado(linha.cells[COLUNA.REALIZADO].innerHTML);
+			linha.cells[COLUNA.DESVIO].innerHTML = "Errado";
+			linha.cells[COLUNA.HISTORICO].innerHTML = "T,N";
+		}
+	} else if (linha.cells[COLUNA.HISTORICO].innerHTML == "T,N") {
+		if (realizado == "T") {
+			linha.cells[COLUNA.PREDITO].innerHTML = realizado;
+			linha.cells[COLUNA.DESVIO].innerHTML = "Certo";
+			linha.cells[COLUNA.ACERTOS].innerHTML++;
+			linha.cells[COLUNA.HISTORICO].innerHTML = "T,T";
+		} else {
+			linha.cells[COLUNA.PREDITO].innerHTML = inverteTomado(linha.cells[COLUNA.REALIZADO].innerHTML);
+			linha.cells[COLUNA.DESVIO].innerHTML = "Errado";
+			linha.cells[COLUNA.HISTORICO].innerHTML = "N,N";
+		}
+	} else if (linha.cells[COLUNA.HISTORICO].innerHTML == "N,N") {
+		if (realizado == "N") {
+			linha.cells[COLUNA.PREDITO].innerHTML = realizado;
+			linha.cells[COLUNA.DESVIO].innerHTML = "Certo";
+			linha.cells[COLUNA.ACERTOS].innerHTML++;
+		} else {
+			linha.cells[COLUNA.PREDITO].innerHTML = inverteTomado(linha.cells[COLUNA.REALIZADO].innerHTML);
+			linha.cells[COLUNA.DESVIO].innerHTML = "Errado";
+			linha.cells[COLUNA.HISTORICO].innerHTML = "N,T";
+		}
+	} else {
+		if (realizado == "N") {
+			linha.cells[COLUNA.PREDITO].innerHTML = realizado;
+			linha.cells[COLUNA.DESVIO].innerHTML = "Certo";
+			linha.cells[COLUNA.ACERTOS].innerHTML++;
+			linha.cells[COLUNA.HISTORICO].innerHTML = "N,N";
+		} else {
+			linha.cells[COLUNA.PREDITO].innerHTML = inverteTomado(linha.cells[COLUNA.REALIZADO].innerHTML);
+			linha.cells[COLUNA.DESVIO].innerHTML = "Errado";
+			linha.cells[COLUNA.HISTORICO].innerHTML = "T,T";
+		}
+	}
+
+	linha.cells[COLUNA.TOTAL].innerHTML++;
+	linha.cells[COLUNA.PRECISAO].innerHTML = calculaPrecisao(linha);
+}
+
+function executar(endereco, realizado) {
+	let enderecoEmBinario = (parseInt(endereco, 16)).toString(2);
+	let indiceBin = enderecoEmBinario.substring(enderecoEmBinario.length - numeroBitsPC, enderecoEmBinario.length);
+	let indiceLinhaTabela = parseInt(indiceBin, 2);
+
+	let linha = $("#myTable")[0].rows[indiceLinhaTabela + 1];
+
+	$(".green").removeClass("green");
+	$(linha).addClass("yellow");
+
+	setTimeout(function () {
+		$(".yellow").removeClass("yellow");
+		$(linha).addClass("green");
+
+		linha.cells[COLUNA.ENDERECO].innerHTML = endereco;
+		linha.cells[COLUNA.REALIZADO].innerHTML = realizado;
+
+		if(numeroBitsPreditor == 1) {
+			preditor1Bit(linha, realizado);
+		} else {
+			preditor2Bits(linha, realizado);
+		}	
+	}, 1000);
 }
