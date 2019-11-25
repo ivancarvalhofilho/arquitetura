cc4bc3689bade3e4406b5de3429d8400a12ef701
ivancarvalhofilho <ivancarvalhofilho@github.com>
2019-11-25 19:07:31 -0200

    Adicionado GHT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let linhasDoArquivo = [];
 let linhaAtualIndice = 0;
 let numeroBitsPreditor;
+let numeroBitsIndexador;
 let numeroBitsPC;
 
 function lerArquivo(){
@@ -15,7 +16,12 @@ function lerArquivo(){
 
 function executarProximaLinha(){
 	linhaAtual = linhasDoArquivo[linhaAtualIndice].split(' ');
-	executar(linhaAtual[0], linhaAtual[1].replace(/\r?\n|\r/, ''));
+	let opcaoPreditorBht = $("[name='preditor']:checked").val();
+	if(opcaoPreditorBht == 0){
+		executarBht(linhaAtual[0], linhaAtual[1].replace(/\r?\n|\r/, ''));
+	} else {
+		executarGht(linhaAtual[0], linhaAtual[1].replace(/\r?\n|\r/, ''));
+	}
 }
 
 function proximaExecutacao(){
@@ -30,15 +36,33 @@ function proximaExecutacao(){
 
 function iniciarPreditor () {
 	lerArquivo();
+
+	numeroBitsPC = $("#numeroBitsPC").val();
+	
+	numeroBitsPreditor = $("#numeroBitsPreditor").val();
+
+	let opcaoPreditorGht = $("[name='preditor']:checked").val();
+	if(opcaoPreditorGht == 1){
+		$("#titulo").text("Preditor Global de Desvios");
+		tamanhoHistoricoGlobal = $("#tamanhoHistoricoGlobal").val();
+		historicoGlobal = "".padStart(tamanhoHistoricoGlobal, "1");
+
+		let tabelaHistoricoGlobal = $("#tabelaHistoricoGlobal")[0];
+		let row = tabelaHistoricoGlobal.insertRow(1);
+		for (let j=0; j < tamanhoHistoricoGlobal; j++) {
+			coluna = row.insertCell(j);
+			coluna.innerHTML = "T";
+		}
+		$(tabelaHistoricoGlobal).show();
+	}
+	numeroBitsIndexador = Number(numeroBitsPC) + Number(tamanhoHistoricoGlobal);
 	
 	$("#menu-1").hide();
 	$("#menu-2").show();
 
 	let table = $("#myTable")[0];
-	numeroBitsPC = $("#numeroBitsPC").val();
-	let quantidadeLinhasNaTabela = Math.pow(2, numeroBitsPC);
+	let quantidadeLinhasNaTabela = Math.pow(2, numeroBitsIndexador);
 	
-	numeroBitsPreditor = $("#numeroBitsPreditor").val();
 
 	let colunas = [];
 	for (let i=0; i < quantidadeLinhasNaTabela; i++){
@@ -48,7 +72,9 @@ function iniciarPreditor () {
 			colunas[j] = row.insertCell(j);
 		}
 		
-		colunas[COLUNA.ID].innerHTML = i.toString(2).padStart(numeroBitsPC, "0");
+		let index = i.toString(2).padStart(numeroBitsIndexador, "0");
+		index = `<global>${index.substring(0, tamanhoHistoricoGlobal)} </global>${index.substring(tamanhoHistoricoGlobal, index.length)}`;
+		colunas[COLUNA.ID].innerHTML = index;
 		colunas[COLUNA.ENDERECO].innerHTML = "";
 		colunas[COLUNA.HISTORICO].innerHTML = numeroBitsPreditor == 1 ? "T" : "T,N";
 		colunas[COLUNA.REALIZADO].innerHTML = "";
@@ -153,9 +179,9 @@ function preditor2Bits(linha, realizado) {
 	linha.cells[COLUNA.PRECISAO].innerHTML = calculaPrecisao(linha);
 }
 
-function executar(endereco, realizado) {
-	let enderecoEmBinario = (parseInt(endereco, 16)).toString(2);
-	let indiceBin = enderecoEmBinario.substring(enderecoEmBinario.length - numeroBitsPC, enderecoEmBinario.length);
+function executarBht(endereco, realizado) {
+	let enderecoEmBinario = ((parseInt(endereco, 16)) >>> 2).toString(2);
+	let indiceBin = enderecoEmBinario.substring(enderecoEmBinario.length - numeroBitsIndexador, enderecoEmBinario.length);
 	let indiceLinhaTabela = parseInt(indiceBin, 2);
 
 	let linha = $("#myTable")[0].rows[indiceLinhaTabela + 1];
@@ -174,6 +200,68 @@ function executar(endereco, realizado) {
 			preditor1Bit(linha, realizado);
 		} else {
 			preditor2Bits(linha, realizado);
-		}	
+		}
+		calculaPrecisaoGeral();
+	}, 250);
+}
+
+let historicoGlobal = null;
+let tamanhoHistoricoGlobal = 0;
+
+function calculaPrecisaoGeral() {
+	let precisaoGeral = 0;
+	let numeroDePreditores = 0;
+	$("#myTable tr").each(function (index, linha) {
+		if(index == 0)
+			return 1;
+		
+		let precisaoAtual = $(linha).find("td")[COLUNA.PRECISAO].innerHTML.replace("%","");
+		if (precisaoAtual != 0) {
+			precisaoGeral += Number(precisaoAtual);
+			numeroDePreditores++;
+		}
+	});
+	if(precisaoGeral != 0){
+		$("#precisaoGeral").text((precisaoGeral/numeroDePreditores).toFixed(2)+"%");
+	}
+}
+
+function executarGht(endereco, realizado) {
+	let enderecoEmBinario = ((parseInt(endereco, 16)) >>> 2).toString(2);
+	let indiceBin = enderecoEmBinario.substring(enderecoEmBinario.length - numeroBitsPC, enderecoEmBinario.length);
+	realizadoNumber = 0;
+	if(realizado == "T"){
+		realizadoNumber = 1;
+	}
+	let historicoGlobalAtual = historicoGlobal + realizadoNumber;
+	historicoGlobalAtual = historicoGlobalAtual.slice(1, historicoGlobalAtual.length);
+	
+	let indiceLinhaTabela = parseInt(historicoGlobal + indiceBin, 2);
+
+	historicoGlobal = historicoGlobalAtual;
+
+	historicoGlobal.split("").forEach(function(item, index){
+		$("#tabelaHistoricoGlobal td")[index].innerHTML = item == 1 ? "T" : "N";
+	});
+	
+	let linha = $("#myTable")[0].rows[indiceLinhaTabela + 1];
+
+	$(".green").removeClass("green");
+	$(".red").removeClass("red");
+	$(linha).addClass("yellow");
+
+	setTimeout(function () {
+		$(".yellow").removeClass("yellow");
+
+		linha.cells[COLUNA.ENDERECO].innerHTML = endereco;
+		linha.cells[COLUNA.REALIZADO].innerHTML = realizado;
+
+		if(numeroBitsPreditor == 1) {
+			preditor1Bit(linha, realizado);
+		} else {
+			preditor2Bits(linha, realizado);
+		}
+		calculaPrecisaoGeral();
 	}, 1000);
 }
+
